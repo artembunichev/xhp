4a986ffd54db46a6ecf7ecec2687f285ac725677
artembunichev <artembunichev@zohomail.com>
2024-09-09 21:46:47 +0300

    increase delay.

--- xhp.c
+++ xhp.c
@@ -13,7 +13,7 @@ dpl=XOpenDisplay(0);
 if(!dpl)return 1;
 w=DefaultRootWindow(dpl);
 dly.tv_sec=0;
-dly.tv_nsec=400000000L;
+dly.tv_nsec=810000000L;
 col=(XColor){0};
 pxmap=XCreatePixmap(dpl,w,1,1,1);
 cur=XCreatePixmapCursor(dpl,pxmap,pxmap,&col,&col,0,0);
