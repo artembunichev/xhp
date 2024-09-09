a73317a743d702c5f55a3ed8f9e031844b17bbfe
artembunichev <artembunichev@zohomail.com>
2024-09-09 21:48:46 +0300

    join lines.

--- xhp.c
+++ xhp.c
@@ -12,8 +12,7 @@ int main(){
 dpl=XOpenDisplay(0);
 if(!dpl)return 1;
 w=DefaultRootWindow(dpl);
-dly.tv_sec=0;
-dly.tv_nsec=810000000L;
+dly.tv_sec=0;dly.tv_nsec=810000000L;
 col=(XColor){0};
 pxmap=XCreatePixmap(dpl,w,1,1,1);
 cur=XCreatePixmapCursor(dpl,pxmap,pxmap,&col,&col,0,0);
