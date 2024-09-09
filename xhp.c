83c721b4ce658fd3d112a79ba79acbfea10ce43b
artembunichev <artembunichev@zohomail.com>
2024-09-09 21:13:08 +0300

    init.

---
+++ xhp.c
@@ -0,0 +1,32 @@
+/*xhp-x hide pointer.*/
+#include<X11/Xlib.h>
+#include<time.h>
+Display* dpl;
+Window w;
+XEvent ev;
+Pixmap pxmap;
+Cursor cur;
+XColor col;
+struct timespec dly;
+int main(){
+dpl=XOpenDisplay(0);
+if(!dpl)return 1;
+w=DefaultRootWindow(dpl);
+dly.tv_sec=0;
+dly.tv_nsec=400000000L;
+col=(XColor){0};
+pxmap=XCreatePixmap(dpl,w,1,1,1);
+cur=XCreatePixmapCursor(dpl,pxmap,pxmap,&col,&col,0,0);
+XFreePixmap(dpl,pxmap);
+while(1){
+if(XGrabPointer(dpl,w,0,PointerMotionMask|ButtonPressMask,GrabModeSync,
+GrabModeAsync,w,cur,CurrentTime)==GrabSuccess){
+	XAllowEvents(dpl,SyncPointer,CurrentTime);
+	do{
+	XMaskEvent(dpl,PointerMotionMask|ButtonPressMask,&ev);
+	XUngrabPointer(dpl,CurrentTime);
+	}while(XPending(dpl));
+	nanosleep(&dly,0);
+}
+}
+return 0;}
